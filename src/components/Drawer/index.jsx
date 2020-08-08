b627c2e1e69f11f2386ebaeb43a7450d3090fdab
Vojtech Cerveny <vojtech.cerveny@cloverdx.com>
2020-08-08 02:26:37 +0200

    drawer update

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Container } from "./styles"
+import { Container, NavItem, Divider } from "./styles"
 
 const Drawer = ({ visibility, setVisibility }) => {
   return (
@@ -12,7 +12,62 @@ const Drawer = ({ visibility, setVisibility }) => {
           transform: visibility ? "none" : "translate( -100% ,0)",
         }}
       >
-        <h1>Drawer</h1>
+        <img id="drawerLogo" src="/images/drawerLogo.png" alt="Logo"  />
+        <NavItem>
+          <div>
+            <img src="/icons/sv.svg" alt="O SV" />
+          </div>
+          <span>SV</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/home.svg" alt="Aktuality" />
+          </div>
+          <span>AKTUALITY</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/harmonogram.svg" alt="Harmonogram" />
+          </div>
+          <span>HARMONOGRAM</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/fish.svg" alt="Fish" />
+          </div>
+          <span>Duchovní PROSTOR</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/chat.svg" alt="Chat" />
+          </div>
+          <span>přednášky</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/activity.svg" alt="aktivity" />
+          </div>
+          <span>AKTIVITY</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/mapa.svg" alt="Mapa" />
+          </div>
+          <span>MAPA</span>
+        </NavItem>
+        <Divider />
+        <NavItem>
+          <div>
+            <img src="/icons/question.svg" alt="Otazecka" />
+          </div>
+          <span>FAQ</span>
+        </NavItem>
       </Container>
       <div
         className="sidenav-overlay"
--- src/components/Drawer/styles.js
+++ src/components/Drawer/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   visibility: hidden;
-  width: 75vw;
+  min-width: 60vw;
   height: 100%;
   position: fixed;
   left: 0px;
@@ -13,4 +13,33 @@ export const Container = styled.div`
   transform: translate(0px, 100%);
   transition: all 4000ms ease-out 4000s;
   background: linear-gradient(191.97deg, #118ce5 48.35%, #23699b 109.28%);
+  flex: 1;
+  color: white;
+  border-radius: 0 10px 10px 0;
+
+  & img#drawerLogo {
+    padding: 30px;
+    width: fit-content;
+    align-self: center;
+  }
+`
+export const NavItem = styled.div`
+  padding: 10px 0 10px 20px;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  height: 44px;
+  & div {
+    width: 50px;
+    text-align: center;
+  }
+  & img {
+    padding-right: 10px;
+  }
+`
+
+export const Divider = styled.div`
+  height: 1px;
+  width: 75%;
+  background: rgba(228, 228, 228, 0.2);
 `
