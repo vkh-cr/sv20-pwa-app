185abb1f978935026701fa74afd5256add76a2be
Vojtech Cerveny <vojtech.cerveny@cloverdx.com>
2020-09-16 01:03:44 +0200

    logo + save url

--- src/constants/static-paragraphs.js
+++ src/constants/static-paragraphs.js
@@ -3,14 +3,14 @@ export default {
     article: {
       header: "Studentský Velehrad 2020",
       text: `<center><img src="images/drawerLogo.png" /></center> <p>   Studentský Velehrad je celostátní setkání vysokoškoláků, které se koná každé   dva roky na známém poutním místě Velehradě. Akci organizuje tým studentů i   pracujících pod záštitou Vysokoškolského katolického hnutí Česká republika a   České biskupské konference. </p> <p>   Pro 15. ročník Studentského Velehradu bylo zvoleno téma:   <b>Něco navíc.</b>Téma má dvě základní roviny – dávat a dostávat. </p> <p>   Chceme žít život naplno? Pak je třeba přijmout výzvu a jít za svým cílem.   Nestačí minimální vklad času, energie a schopností. Jestliže to má stát za to,   musíme <b>dát</b> něco navíc… Vyplout z komfortní zóny našeho přístavu a někdy   zase naopak zůstat zakotvení tam, kde jsme. Dát něco navíc neznamená hned   velká gesta – někdy stačí pouhý úsměv, drobná pomoc či rozhovor. Každý z nás   ví, kde má své rezervy – jestli je to např. ochrana životního prostředí, větší   občanská angažovanost nebo lepší organizace času a důraz na vlastní odpočinek… </p> <p>   Neméně důležité v životě je také <b>přijímat </b>. Každý z nás dostal něco   navíc. Jsou to jedinečné dary – předpoklady, schopnosti a touhy. Objevit je a   snažit se je co nejlépe využít, to už je však na každém z nás! </p> <center><img src="images/drawerLogo.png" /></center> <h1>Co je to VKH ČR?</h1> <p>   Vysokoškolské katolické hnutí Česká republika, z. s. (VKH ČR) je organizace   sdružující jednotlivá společenství mladých věřících v univerzitních městech   České republiky – VKH v Praze, Brně, Olomouci, Ostravě, Českých Budějovicích,   Akademický klub Salaš v Hradci Králové, a RR :09.ve Zlíně. </p> <p>   Spolek je finančně podporován Českou biskupskou konferencí, která mu   přenechává volnost ve službě mladým vysokoškolského věku. Hlavním posláním VKH   ČR je podpora a utužování vztahů mezi spolky v jednotlivých městech.   Každoročně pořádá konferenci, která slouží ke sdílení inspirace a nápadů, a   jednou za dva roky organizuje Studentský či Absolventský Velehrad (:021).   Tento spolek již přes dvacet let nabízí prostor k duchovnímu růstu, rozšíření   obzorů a k aktivnímu využití volného času. </p> <p>   Statutárním orgánem spolku je zvolený prezident, jenž má dvouleté funkční   období a tradičně se volí během SV. Prezident se zabývá činnostmi spojenými   s vedením spolku, odpovídá za reprezentaci na veřejnosti a je kontaktní osobou   pro členy i pro spřátelené spolky či hnutí. Současnou prezidentkou je Alžběta   Pavlačková, kterou lze kontaktovat na emailu-prezident@vkhcr.org. </p> <p>   NOVÝ(Á) prezident/ka bude zvolen/a valnou hromadou v neděli v čase :3::0 –   15:00 v prostorách gymnázia. Zasedání je volně přístupné a představení   zvoleného prezidenta proběhne při mši svaté. </p> `,
-      imageSrc: "http://studentskyvelehrad.cz/images/headers/spolecne-momenty.jpg",
+      imageSrc: "/images/prayingCubes/1.png",
     },
   },
 
   faq: {
     article: {
       header: "FAQ",
-      imageSrc: "http://studentskyvelehrad.cz/images/headers/spolecne-momenty.jpg",
+      imageSrc: "/images/prayingCubes/2.png",
       text: `
       <h1>Důležité informace</h1>
       <h2>Kontakty</h2>
